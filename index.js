b6b867945df6d1b94bdef697d66f37b7b015d4dd
tashantukin <nmfnavarro@gmail.com>
2021-08-08 07:16:53 +0800

    the Object Literal pattern

--- index.js
+++ index.js
@@ -1,46 +1,50 @@
-var leadSelf='Me: ',
-leadComputer = 'PC: ',
-aSaid =["This is a cyber chat!"],
-msgYes = "yes, that's a great idea.",
-msgNo = "No, that must be a mistake",
-aSassyStuff = ["Like mold ob books, grow myths and history",
-            "She moved like a poem and smiled like a sphinx",
-            "As long as we dont die, this is gonna be one hell of a story",
-            "She laughed, and the desert sang",
-            "You've got about a charm as much as a dead slug"]
 
-function talk(msg) {
-    echo(leadSelf +  msg)
-}
+var o = {
+    leadSelf:'Me: ',
+    leadComputer:'PC: ',
+    aSaid:["This is a cyber chat!"],
+    msgYes:"yes, that's a great idea.",
+    msgNo:"No, that must be a mistake",
+    aSassyStuff: ["Like mold ob books, grow myths and history",
+                "She moved like a poem and smiled like a sphinx",
+                "As long as we dont die, this is gonna be one hell of a story",
+                "She laughed, and the desert sang",
+                "You've got about a charm as much as a dead slug"],
 
-function replyYesNo(){
-    var msg =  Math.random() > .5 ? msgYes : msgNo
-    echo (leadComputer + msg)
-}
+     talk: function(msg) {
+            this.echo(this.leadSelf +  msg)
+     },
 
-function saySassyStuff(){
+    replyYesNo: function(){
+          var msg =  Math.random() > .5 ? this.msgYes : this.msgNo
+          echo (this.leadComputer + msg)
+    },
 
-    var msg =  aSassyStuff[Math.floor(Math.random() * aSassyStuff.length)];
+    saySassyStuff: function(){
+        var msg =  this.aSassyStuff[Math.floor(Math.random() * this.aSassyStuff.length)];
 
-    echo (leadComputer + msg)
-}
+        echo (this.leadComputer + msg)
+    },
+
+    echo: function(msg){
+        this.aSaid.push(`<div> ${msg} </div>`)
+
+        var  aSaidLength = this.aSaid.length,
+            start = Math.max(aSaidLength - 6,0),
+            out = "";
+
+        for(var i=start; i<aSaidLength; i++) {
+            out += this.aSaid[i];
+        }
+
+        $('.advert').html(out);
 
 
-
-function echo(msg) {
-
-    aSaid.push(`<div> ${msg} </div>`)
-
-    var  aSaidLength = aSaid.length,
-         start = Math.max(aSaidLength - 6,0),
-         out = "";
-
-    for(var i=start; i<aSaidLength; i++) {
-        out += aSaid[i];
+        $('#talk span').text(msg)
     }
 
-    $('.advert').html(out);
 
 
-    $('#talk span').text(msg)
 }
+
+
